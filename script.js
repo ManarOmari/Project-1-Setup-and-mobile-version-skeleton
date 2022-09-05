c984314dfea7aa7ef4f44600b6d652b296a7692c
Sajeel Zafar <sajeelzafar1995@gmail.com>
2022-09-05 17:22:17 +0500

    Created mobile menu using JS

---
+++ script.js
@@ -0,0 +1,13 @@
+const hamburger = document.querySelector('.humburger');
+const navMenu = document.querySelector('.navMenu');
+
+hamburger.addEventListener('click', () => {
+  hamburger.classList.toggle('active');
+  navMenu.classList.toggle('active');
+});
+
+document.querySelectorAll('.navMenu').forEach((n) => (
+  n.addEventListener('click', () => {
+    hamburger.classList.remove('active');
+    navMenu.classList.remove('active');
+  })));
