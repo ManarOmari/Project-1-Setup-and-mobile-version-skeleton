e8ab3b80207235772f92521447a300331faea365
Manar <manar.qasem732016@gmail.com>
2022-09-06 17:44:11 +0300

    update the form validation

--- form_validation.js
+++ form_validation.js
@@ -1,14 +1,16 @@
-const submitBtn = document.querySelector('.seeProject');
+const submitBtn = document.querySelector('#seeProject');
 const validateEmail = (email) => {
   if (email.match(/^[a-z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-z0-9-]+(?:\.[a-z0-9-]+)*$/)) {
     return true;
+   
   }
-  return false;
+    return false;
 };
 
-submitBtn.addEventListener('click', () => {
+submitBtn.addEventListener('click', (event) => {
   const emailVal = document.getElementById('email').value;
   if (!validateEmail(emailVal)) {
-    document.querySelector('.txtt').innerHTML = ' please enter correct email in lower case eg abcde@fgh.com';
+    let label=document.querySelector('.label').innerHTML="Email should be wriitn in small case";
+    event.preventDefault();
   }
 });
