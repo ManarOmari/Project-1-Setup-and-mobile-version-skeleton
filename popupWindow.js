6aa518fbc19dfd4b3cb2d24302d6859689f1d077
Manar <manar.qasem732016@gmail.com>
2022-09-08 17:25:32 +0300

    update projectList object

--- popupWindow.js
+++ popupWindow.js
@@ -12,7 +12,7 @@ const projectList = {
       "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent",
     featured_image: new Image(),
     technologies: ["Ruby on rails", "css", "Javascript"],
-    technologiesDesktop: ["Ruby on rails", "css", "Javascript"],
+    technologiesDesktop: ["Codekit", "GitHub", "Javascript","Bootstrap","Terminal","Codepen"],
     liveBtnImge: new Image(),
     SourceBtn: new Image(),
   },
@@ -22,7 +22,8 @@ document.querySelectorAll("#seeProjectPopup").forEach((n) =>
   n.addEventListener("click", () => {
     // check if screen is for desktop
     if(x.matches){
-       let y=1;
+      // desktop 
+       
       }
        else {
      
