a4b2dbcd22b4823e11e6bea7c926ec5fd92e5d06
Manar <manar.qasem732016@gmail.com>
2022-09-08 15:58:08 +0300

    Add btns

--- popupWindow.js
+++ popupWindow.js
@@ -19,52 +19,68 @@ const projectList = {
 document.querySelectorAll("#seeProjectPopup").forEach((n) =>
   n.addEventListener("click", () => {
     modal_container.style.display = "block";
-    modal_container.classList.add('multi-stories');
+    modal_container.classList.add("multi-stories");
     let div = document.createElement("div");
-    div.classList.add('card');
+    div.classList.add("card");
     //let closeBtn = document.createElement("span");
     //closeBtn.classList.add("close");
-   // closeBtn.innerHTML = "&times;";
-    let image=document.createElement('img');
-     image.innerHTML = projectList.project1.featured_image;
-     image.src='/assets/images/bg1.png';
-    let cardConatiner= document.createElement('div');
-    cardConatiner.classList.add('cardContainer');
+    // closeBtn.innerHTML = "&times;";
+    let image = document.createElement("img");
+    image.innerHTML = projectList.project1.featured_image;
+    image.src = "/assets/images/bg1.png";
+    let cardConatiner = document.createElement("div");
+    cardConatiner.classList.add("cardContainer");
     let heder = document.createElement("h2");
+    heder.classList.add("titleHeader");
     heder.innerHTML = projectList.project1.title;
-
     let listOfBtns = document.createElement("ul");
-    listOfBtns.classList.add('projectBTNs');
-    let listItem= document.createElement('li');
-    
-    for(let i=0;i<projectList.project1.technologies.length;i+=1){
-      let btns=document.createElement('button');
-      btns.className= '.projectBTNs li button';
-      btns.innerHTML=projectList.project1.technologies[i];
+    listOfBtns.classList.add("projectBTNs");
+    let listItem = document.createElement("li");
+
+    for (let i = 0; i < projectList.project1.technologies.length; i += 1) {
+      let btns = document.createElement("button");
+      btns.className = ".projectBTNs li button";
+      btns.innerHTML = projectList.project1.technologies[i];
       listItem.append(btns);
       listOfBtns.appendChild(listItem);
     }
-   let text= document.createElement('p');
-   //text.classList.add('text');
-   text.innerHTML=projectList.project1.description;
-     
-// When the user clicks on image x, close the modal
-   image.onclick=function(e) {
+    let text = document.createElement("p");
+    text.classList.add("textModal");
+    text.innerHTML = projectList.project1.description;
+    // btn source and live btns
+    let listOfBtns2 = document.createElement("ul");
+    listOfBtns2.classList.add("projectBTNs");
+    // live btn
+    let listItem1 = document.createElement("li");
+    let liveBtn = document.createElement("button");
+    liveBtn.classList.add('livebtns');
+    liveBtn.innerHTML = "See Live";
+    listItem1.append(liveBtn);
+    listOfBtns2.appendChild(listItem1);
+     // source btn
+     let listItem2 = document.createElement("li");
+     let sourceBtn = document.createElement("button");
+     sourceBtn.classList.add('sourcebtns');
+     sourceBtn.innerHTML = "See Source";
+     listItem2.append(sourceBtn);
+     listOfBtns2.appendChild(listItem2);
+
+    // When the user clicks on image x, close the modal
+    image.onclick = function (e) {
       modal.style.display = "none";
     };
     //div.appendChild(closeBtn);
     div.appendChild(image);
     modal.appendChild(div);
     cardConatiner.appendChild(heder);
-   
     cardConatiner.appendChild(listOfBtns);
     cardConatiner.appendChild(text);
+    cardConatiner.appendChild(listOfBtns2);
     modal.appendChild(cardConatiner);
     modal_container.appendChild(modal);
     document.body.appendChild(modal_container);
   })
 );
-
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function (event) {
