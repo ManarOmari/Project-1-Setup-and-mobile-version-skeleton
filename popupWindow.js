f64093905090e4e5f6a66aff600d4a2703ca84a2
Manar <manar.qasem732016@gmail.com>
2022-09-09 11:03:04 +0300

    update popup javascript

--- popupWindow.js
+++ popupWindow.js
@@ -94,62 +94,8 @@ document.querySelectorAll("#seeProjectPopup").forEach((n) =>
       // desktop 
        
       }
-       else {
-     
-    modal_container.style.display = "block";
-    modal_container.classList.add("multi-stories");
-    let div = document.createElement("div");
-    div.classList.add("card");
-    //let closeBtn = document.createElement("span");
-    //closeBtn.classList.add("close");
-    // closeBtn.innerHTML = "&times;";
-    let image = document.createElement("img");
-    image.innerHTML = projectList.project1.featured_image;
-    image.src = "/assets/images/bg1.png";
-    let cardConatiner = document.createElement("div");
-    cardConatiner.classList.add("cardContainer");
-    let heder = document.createElement("h2");
-    heder.classList.add("titleHeader");
-    heder.innerHTML = projectList.project1.title;
-    let listOfBtns = document.createElement("ul");
-    listOfBtns.classList.add("projectBTNs");
-    let listItem = document.createElement("li");
-
-    for (let i = 0; i < projectList.project1.technologies.length; i += 1) {
-      let btns = document.createElement("button");
-      btns.className = ".projectBTNs li button";
-      btns.innerHTML = projectList.project1.technologies[i];
-      listItem.append(btns);
-      listOfBtns.appendChild(listItem);
-    }
-    let text = document.createElement("p");
-    text.classList.add("textModal");
-    text.innerHTML = projectList.project1.description;
-    // btn source and live btns
-    let listOfBtns2 = document.createElement("ul");
-    listOfBtns2.classList.add("projectBTNs");
-    // live btn
-    let listItem1 = document.createElement("li");
-    let liveBtn = document.createElement("button");
-    liveBtn.classList.add('livebtns');
-    liveBtn.innerHTML = "See Live";
-    listItem1.append(liveBtn);
-    listOfBtns2.appendChild(listItem1);
-     // source btn
-     let listItem2 = document.createElement("li");
-     let sourceBtn = document.createElement("button");
-     sourceBtn.classList.add('sourcebtns');
-     sourceBtn.innerHTML = "See Source";
-     listItem2.append(sourceBtn);
-     listOfBtns2.appendChild(listItem2);
-
-    // When the user clicks on image x, close the modal
-    image.addEventListener('click', () => {
-      modal_container.style.display = "none";
-    });
->>>>>>> 39260e4fe803a3bb3a4b6ef6b7fa21ef5e090a83
-    
-    } else {
+      
+    else {
       modal_container.style.display = "block";
       modal_container.classList.add("multi-stories");
       let div = document.createElement("div");
