748fcb1f26e448c3170bc1d3023dfbea1de29c94
Manar <manar.qasem732016@gmail.com>
2022-09-09 14:32:28 +0300

    update index

--- popupWindow.js
+++ popupWindow.js
@@ -4,7 +4,8 @@ const modal = document.querySelector('.modal');
 const x = window.matchMedia('(min-width: 768px)'); // desktop
 // array to store element
 const projectList = {
-  project1: {
+  project1:
+   {
     title: 'Keeping track of hundreds of components',
     description:
       "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent",
